88ae14de89665d4560427ebece8d8736dcdba534
Omar Suca <omar@colosa.com>
2014-04-12 21:30:42 -0400

    Working more on styles

--- www/js/index.js
+++ www/js/index.js
@@ -3,3 +3,9 @@ startGame = function () {
     alert("You started the game");
     Reveal.next();
 };
+goToSlide = function (layer) {
+    Reveal.slide(layer);
+};
+setTimeout(function() {
+    jQuery(".layout-loading").css("display","none");    
+}, 5000);
